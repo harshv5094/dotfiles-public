5b83270d32eec2538891ee7f32fe796b02ee4e72
Harsh Vyapari (Local) <harshv5094@gmail.com>
2024-06-10 08:15:09 +0530

    refactor(install.sh): extracting all cmd check into a single file
    I've created a script file to check if my last command is successfully executed or not, It's getting
    quite hassle to maintain my script file so I design this script to do it for me. It'll allow me to
    smoothly add my folders and dotfiles in the config file

--- .scripts/modules/config_folders.sh
+++ .scripts/modules/config_folders.sh
@@ -1,189 +1,129 @@
 #!/usr/bin/env bash
 
 source ~/.dotfiles/.scripts/modules/colors.sh
+source ~/.dotfiles/.scripts/modules/cmd_check.sh
 
 # nvim
 if [ -d "$HOME/.config/nvim" ]; then
 	echo_danger "Deleting Existing Nvim Config Folder"
+
 	rm -rf "$HOME/.config/nvim"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted Nvim Config Folder"
-	else
-		echo_error "Failed to delete Nvim Config Folder"
-	fi
+	delete_folder_check "Nvim"
 	echo_info "Linking Nvim Config Folder"
+
 	ln -s "$HOME/.dotfiles/.config/nvim" "$HOME/.config/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Nvim Config Folder"
-	else
-		echo_error "Failed to link Nvim Config Folder"
-	fi
+	link_folder_check "Nvim"
 else
 	echo_info "Linking Nvim Config Folder"
+
 	ln -s "$HOME/.dotfiles/.config/nvim" "$HOME/.config/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Nvim Config Folder"
-	else
-		echo_error "Failed to link Nvim Config Folder"
-	fi
+	link_folder_check "Nvim"
 fi
 
 # doom emacs
 if [ -d "$HOME/.config/doom" ]; then
 	echo_danger "Deleting Existing doom Config Folder"
+
 	rm -rf "$HOME/.config/doom"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted doom Config Folder"
-	else
-		echo_error "Failed to delete doom Config Folder"
-	fi
+	delete_folder_check "doom"
 	echo_info "Linking doom Config Folder"
+
 	ln -s "$HOME/.dotfiles/.config/doom" "$HOME/.config/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked doom Config Folder"
-	else
-		echo_error "Failed to link doom Config Folder"
-	fi
+	link_folder_check "doom"
 else
 	echo_info "Linking doom Config Folder"
+
 	ln -s "$HOME/.dotfiles/.config/doom" "$HOME/.config/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked doom Config Folder"
-	else
-		echo_error "Failed to link doom Config Folder"
-	fi
+	link_folder_check "doom"
 fi
 
 # tmux
 if [ -d "$HOME/.config/tmux" ]; then
 	echo_danger "Deleting Existing Tmux Config Folder"
 	rm -rf "$HOME/.config/tmux"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted Tmux Config Folder"
-	else
-		echo_error "Failed to delete Tmux Config Folder"
-	fi
+
+	delete_folder_check "Tmux"
 	echo_info "Linking Tmux Config Folder"
+
 	ln -s "$HOME/.dotfiles/.config/tmux" "$HOME/.config/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Tmux Config Folder"
-	else
-		echo_error "Failed to link Tmux Config Folder"
-	fi
+	link_folder_check "Tmux"
 else
 	echo_info "Linking Tmux Config Folder"
+
 	ln -s "$HOME/.dotfiles/.config/tmux" "$HOME/.config/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Tmux Config Folder"
-	else
-		echo_error "Failed to link Tmux Config Folder"
-	fi
+	link_folder_check "Tmux"
 fi
 
 # kitty
 if [ -e "$HOME/.config/kitty" ]; then
 	echo_danger "Deleting Existing Kitty Config"
 	rm -rf "$HOME/.config/kitty"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted Kitty Config"
-	else
-		echo_error "Failed to delete Kitty Config"
-	fi
+
+	delete_folder_check "Kitty"
 	echo_info "Linking Kitty Config"
+
 	ln -s "$HOME/.dotfiles/.config/kitty" "$HOME/.config/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Kitty Config"
-	else
-		echo_error "Failed to link Kitty Config"
-	fi
+	link_folder_check "Kitty"
 else
 	echo_info "Linking Kitty Config"
+
 	ln -s "$HOME/.dotfiles/.config/kitty" "$HOME/.config/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Kitty Config"
-	else
-		echo_error "Failed to link Kitty Config"
-	fi
+	link_folder_check "Kitty"
 fi
 
 # alacritty
 if [ -e "$HOME/.config/alacritty" ]; then
 	echo_danger "Deleting Existing Alacritty Config"
 	rm -rf "$HOME/.config/alacritty"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted Alacritty Config"
-	else
-		echo_error "Failed to delete Alacritty Config"
-	fi
-	echo_info "Linking alacritty Config"
+
+	delete_folder_check "Alacritty"
+	echo_info "Linking Alacritty Config"
+
 	ln -s "$HOME/.dotfiles/.config/alacritty" "$HOME/.config/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Alacritty Config"
-	else
-		echo_error "Failed to link Alacritty Config"
-	fi
+	link_folder_check "Alacritty"
 else
 	echo_info "Linking alacritty Config"
+
 	ln -s "$HOME/.dotfiles/.config/alacritty" "$HOME/.config/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Alacritty Config"
-	else
-		echo_error "Failed to link Alacritty Config"
-	fi
+	link_folder_check "Alacritty"
 fi
 
 # lazygit
 if [ -e "$HOME/.config/lazygit/config.yml" ]; then
 	echo_danger "Deleting Existing Lazygit Config"
 	rm -rf "$HOME/.config/lazygit"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted Lazygit Config"
-	else
-		echo_error "Failed to delete Lazygit Config"
-	fi
+
+	delete_folder_check "Lazygit"
 	echo_info "Linking Lazygit Config"
+
 	mkdir -p "$HOME/.config/lazygit/"
 	ln -s "$HOME/.dotfiles/.config/lazygit/config.yml" "$HOME/.config/lazygit/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Lazygit Config"
-	else
-		echo_error "Failed to link Lazygit Config"
-	fi
+	link_folder_check "Lazygit"
 else
 	echo_info "Linking Lazygit Config"
+
 	mkdir -p "$HOME/.config/lazygit/"
 	ln -s "$HOME/.dotfiles/.config/lazygit/config.yml" "$HOME/.config/lazygit/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Lazygit Config"
-	else
-		echo_error "Failed to link Lazygit Config"
-	fi
+
+	link_folder_check "Lazygit"
 fi
 
 # Btop
 if [ -e "$HOME/.config/btop/btop.conf" ]; then
 	echo_danger "Deleting Existing Btop Config"
 	rm -rf "$HOME/.config/btop"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted Btop Config"
-	else
-		echo_error "Failed to delete Lazygit Config"
-	fi
+
+	delete_folder_check "Btop"
 	echo_info "Linking Btop Config"
+
 	mkdir -p "$HOME/.config/btop/"
 	ln -s "$HOME/.dotfiles/.config/btop/btop.conf" "$HOME/.config/btop/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Btop Config"
-	else
-		echo_error "Failed to link Btop Config"
-	fi
+	link_folder_check "Btop"
 else
 	echo_info "Linking Lazygit Config"
+
 	mkdir -p "$HOME/.config/btop/"
 	ln -s "$HOME/.dotfiles/.config/btop/btop.conf" "$HOME/.config/btop/"
-	if [ $? -eq 0 ]; then
-		echo_success "Linked Btop Config"
-	else
-		echo_error "Failed to link Btop Config"
-	fi
+
+	link_folder_check "Btop"
 fi
--- .scripts/modules/dotfiles.sh
+++ .scripts/modules/dotfiles.sh
@@ -1,185 +1,109 @@
 #!/usr/bin/env bash
 
 source ~/.dotfiles/.scripts/modules/colors.sh
+source ~/.dotfiles/.scripts/modules/cmd_check.sh
 
 # .bashrc
 if [ -e "$HOME/.bashrc" ]; then
 	echo_danger "Deleting Existing .bashrc"
 	rm "$HOME/.bashrc"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted .bashrc"
-	else
-		echo_error "Failed to delete .bashrc"
-	fi
+	delete_file_check ".bashrc"
+
 	echo_info "Linking .bashrc"
 	ln -s "$HOME/.dotfiles/dot/.bashrc" "$HOME/"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully linked .bashrc"
-	else
-		echo_error "Failed to link .bashrc"
-	fi
+	link_file_check ".bashrc"
 else
 	echo_info "Linking .bashrc"
 	ln -s "$HOME/.dotfiles/dot/.bashrc" "$HOME/"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully linked .bashrc"
-	else
-		echo_error "Failed to link .bashrc"
-	fi
+	delete_file_check ".bashrc"
 fi
 
 # .zshrc
 if [ -e "$HOME/.zshrc" ]; then
 	echo_danger "Deleting Existing .zshrc"
 	rm "$HOME/.zshrc"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted .zshrc"
-	else
-		echo_error "Failed to delete .zshrc"
-	fi
+	delete_file_check ".zshrc"
+
 	echo_info "Linking .zshrc"
 	ln -s "$HOME/.dotfiles/dot/.zshrc" "$HOME/"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully linked .zshrc"
-	else
-		echo_error "Failed to link .zshrc"
-	fi
+	link_file_check ".zshrc"
 else
 	echo_info "Linking .zshrc"
 	ln -s "$HOME/.dotfiles/dot/.zshrc" "$HOME/"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully linked .zshrc"
-	else
-		echo_error "Failed to link .zshrc"
-	fi
+	link_file_check ".zshrc"
 fi
 
 # .bash_aliases
 if [ -e "$HOME/.bash_aliases" ]; then
 	echo_danger "Deleting Existing .bash_aliases"
 	rm "$HOME/.bash_aliases"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted .bash_aliases"
-	else
-		echo_error "Failed to delete .bash_aliases"
-	fi
+	delete_file_check ".bash_aliases"
+
 	echo_info "Linking .bash_aliases"
 	ln -s "$HOME/.dotfiles/dot/.bash_aliases" "$HOME/.bash_aliases"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully linked .bash_aliases"
-	else
-		echo_error "Failed to link .bash_aliases"
-	fi
+	link_file_check ".bash_aliases"
 else
 	echo_info "Linking .bash_aliases"
 	ln -s "$HOME/.dotfiles/dot/.bash_aliases" "$HOME/.bash_aliases"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully linked .bash_aliases"
-	else
-		echo_error "Failed to link .bash_aliases"
-	fi
+	link_file_check ".bash_aliases"
 fi
 
 # .zsh_aliases
 if [ -e "$HOME/.zsh_aliases" ]; then
 	echo_danger "Deleting Existing .zsh_aliases"
 	rm "$HOME/.zsh_aliases"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted .zsh_aliases"
-	else
-		echo_error "Failed to delete .zsh_aliases"
-	fi
+	delete_file_check ".zsh_aliases"
+
 	echo_info "Linking .zsh_aliases"
 	ln -s "$HOME/.dotfiles/dot/.zsh_aliases" "$HOME/.zsh_aliases"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully linked .zsh_aliases"
-	else
-		echo_error "Failed to link .zsh_aliases"
-	fi
+	link_file_check ".zsh_aliases"
 else
 	echo_info "Linking .zsh_aliases"
 	ln -s "$HOME/.dotfiles/dot/.zsh_aliases" "$HOME/.zsh_aliases"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully linked .zsh_aliases"
-	else
-		echo_error "Failed to link .zsh_aliases"
-	fi
+	link_file_check ".zsh_aliases"
 fi
 
 # .gitconfig
 if [ -e "$HOME/.gitconfig" ]; then
 	echo_danger "Deleting Existing .gitconfig"
 	rm "$HOME/.gitconfig"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted .gitconfig"
-	else
-		echo_error "Failed to delete .gitconfig"
-	fi
-	echo_info "Copying .gitconfig"
-	cp "$HOME/.dotfiles/dot/.gitconfig" "$HOME/"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully copied .gitconfig"
-	else
-		echo_error "Failed to copy .gitconfig"
-	fi
-else
-	echo_info "Copying .gitconfig"
-	cp "$HOME/.dotfiles/dot/.gitconfig" "$HOME/"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully copied .gitconfig"
-	else
-		echo_error "Failed to copy .gitconfig"
-	fi
-fi
+	delete_file_check ".gitconfig"
 
-# .Xmodmap
-if [ -e "$HOME/.Xmodmap" ]; then
-	echo_danger "Deleting Existing .Xmodmap"
-	rm "$HOME/.Xmodmap"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted .Xmodmap"
-	else
-		echo_error "Failed to delete .Xmodmap"
-	fi
-	echo_info " Copying .Xmodmap"
-	cp "$HOME/.dotfiles/dot/.Xmodmap" "$HOME/"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully copied .Xmodmap"
-	else
-		echo_error "Failed to copy .Xmodmap"
-	fi
+	echo_info "Copying .gitconfig"
+	cp "$HOME/.dotfiles/dot/.gitconfig" "$HOME/"
+	copy_file_check ".gitconfig"
 else
-	echo_info "Copying .Xmodmap"
-	cp "$HOME/.dotfiles/dot/.Xmodmap" "$HOME/"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully copied .Xmodmap"
-	else
-		echo_error "Failed to copy .Xmodmap"
-	fi
+	echo_info "Copying .gitconfig"
+	cp "$HOME/.dotfiles/dot/.gitconfig" "$HOME/"
+	copy_file_check ".gitconfig"
 fi
 
 # .czrc
 if [ -e "$HOME/.czrc" ]; then
 	echo_danger "Deleting Existing .czrc"
 	rm "$HOME/.czrc"
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted .czrc"
-	else
-		echo_error "Failed to delete .czrc"
-	fi
+	delete_file_check ".czrc"
+
 	echo_info " Copying .czrc"
 	cp "$HOME/.dotfiles/dot/.czrc" "$HOME/"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully copied .czrc"
-	else
-		echo_error "Failed to copy .czrc"
-	fi
+	copy_file_check ".czrc"
 else
 	echo_info "Copying .czrc"
 	cp "$HOME/.dotfiles/dot/.czrc" "$HOME/"
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully copied .czrc"
-	else
-		echo_error "Failed to copy .czrc"
-	fi
+	copy_file_check ".czrc"
+fi
+
+#.vimrc
+if [ -e "$HOME/.vimrc" ]; then
+	echo_danger "Deleting Existing .vimrc"
+	rm "$HOME/.vimrc"
+	delete_file_check ".vimrc"
+
+	echo_info "Copying .vimrc"
+	cp "$HOME/.dotfiles/dot/.vimrc" "$HOME/"
+	copy_file_check ".vimrc"
+else
+	echo_info "Copying .vimrc"
+	cp "$HOME/.dotfiles/dot/.vimrc" "$HOME/"
+	copy_file_check ".vimrc"
 fi
