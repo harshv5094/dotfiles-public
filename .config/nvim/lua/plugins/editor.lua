7ef9b3914da83234e8ddcb4642d83d26388c6455
Harsh Vyapari (Fedora) <harshv5094@gmail.com>
2024-06-20 22:27:30 +0530

    feat(nvim): enable Orgmode Config

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -286,9 +286,32 @@ return {
 		end,
 	},
 
+	-- For Vim Practice
 	{
 		"theprimeagen/vim-be-good",
 		lazy = true,
 		event = "VeryLazy",
+	},
+
+	-- Org Mode in Neovim
+
+	{
+		"nvim-orgmode/orgmode",
+		event = "VeryLazy",
+		ft = { "org" },
+		config = function()
+			-- Setup orgmode
+			require("orgmode").setup({
+				org_agenda_files = "~/orgfiles/**/*",
+				org_default_notes_file = "~/orgfiles/refile.org",
+			})
+
+			-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+			-- add ~org~ to ignore_install
+			-- require('nvim-treesitter.configs').setup({
+			--   ensure_installed = 'all',
+			--   ignore_install = { 'org' },
+			-- })
+		end,
 	},
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
 				-- "dockerfile",
 				"python",
 				"html",
+				"org",
 				"cpp",
 				"css",
 				"vimdoc",
