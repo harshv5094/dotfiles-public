f00a6a18c6b34776bb8b0d8239b44ee2b456a21a
Harsh Vyapari (fedora PC) <harshv5094@gmail.com>
2024-06-21 19:31:28 +0530

    fix(nvm.sh): elevating installation permission
    Since it needs sudo to install package, I'm giving it the permission

--- .scripts/modules/nvm.sh
+++ .scripts/modules/nvm.sh
@@ -3,5 +3,5 @@
 if type -q nvm
   nvm install lts
   echo "lts" >> ~/.nvmrc
-  npm i -g commitizen cz-conventional-changelog yarn bun
+  sudo npm i -g commitizen cz-conventional-changelog yarn bun
 end
