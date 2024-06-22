280707f988789be4ceddd8661100247df14acae4
Harsh Vyapari (fedora PC) <harshv5094@gmail.com>
2024-06-22 15:07:37 +0530

    fix(ide.sh): removing my modifications

--- .scripts/modules/ide.sh
+++ .scripts/modules/ide.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# Prompt the user to enter a directory path
-read -p "Enter directory path (or press Enter to use the current directory): " dir_path
-
-# Use the current directory if no path is provided
-if [ -z "$dir_path" ]; then
-	dir_path=$(pwd)
-fi
-
-# Check if the directory exists
-if [ ! -d "$dir_path" ]; then
-	echo "Invalid directory path"
-	exit 1
-fi
-
-# Create a new tmux session and navigate to the specified directory
-tmux new-session -d -s ide -c "$dir_path"
-
-# Execute tmux commands within the new session
-tmux send-keys -t ide "tmux split-window -v -l 30%" C-m
-# tmux send-keys -t ide "tmux split-window -h -l 66%" C-m
-tmux send-keys -t ide "tmux split-window -h -l 50%" C-m
-
-# Attach to the new session
-tmux attach -t ide
+tmux split-window -v -l 30%
+tmux split-window -h -l 66%
+tmux split-window -h -l 50%
