8b8f40d74d2d6e227a06194665e1f6f0cab9da6b
Harsh Vyapari (fedora PC) <harshv5094@gmail.com>
2024-06-26 20:41:16 +0530

    feat: adding dunst and other packages

--- .scripts/modules/config_folders.sh
+++ .scripts/modules/config_folders.sh
@@ -181,3 +181,18 @@ else
 
 	link_folder_check "Yazi"
 fi
+
+# dunst
+if [ -e "$HOME/.config/dunst" ]; then
+	echo_danger "Deleting Existing dunst Config"
+	rm -rf "$HOME/.config/dunst"
+	delete_folder_check "dunst"
+
+	echo_info "Linking dunst Config"
+	ln -s "$HOME/GitHub/hyprland-setup/.config/dunst" "$HOME/.config/"
+	link_folder_check "dunst"
+else
+	echo_info "Linking dunst Config"
+	ln -s "$HOME/GitHub/hyprland-setup/.config/dunst" "$HOME/.config/"
+	link_folder_check "dunst"
+fi
